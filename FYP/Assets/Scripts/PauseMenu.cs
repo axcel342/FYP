a5c4487e1df461844e6595701923308535509bb0
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2024-02-20 10:41:32 -0500

    Added Pause Menu mechanic to puzzle screen

---
+++ FYP/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
